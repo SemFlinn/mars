eb94b12e7007a596b25ad8704cee2ad5ac65c7af
SemFlinn <100510929+SemFlinn@users.noreply.github.com>
2022-09-10 12:53:06 +0300

    Update swiper.js

--- js/swiper.js
+++ js/swiper.js
@@ -1,20 +1,3 @@
-const swiper1 = new Swiper('.swiper1', {
-  // Optional parameters
-  direction: 'vertical',
-  loop: true,
-
-  // If we need pagination
-  pagination: {
-    el: '.swiper1-pagination',
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper1-scrollbar',
-  },
-});
-
-
 
 const swiper2 = new Swiper('.swiper2', {
   // Optional parameters
