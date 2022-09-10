8afeea10d6089cf7a6b4d29c69bd7bcf1fc62e75
SemFlinn <100510929+SemFlinn@users.noreply.github.com>
2022-09-10 12:52:01 +0300

    Update swiper.js

--- js/swiper.js
+++ js/swiper.js
@@ -16,31 +16,6 @@ const swiper1 = new Swiper('.swiper1', {
 
 
 
-const swiper11 = new Swiper('.swiper11', {
-  // Optional parameters
-  direction: 'vertical',
-  loop: true,
-
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
-});
-
-
-
-
 const swiper2 = new Swiper('.swiper2', {
   // Optional parameters
   direction: 'horizontal',
@@ -136,43 +111,6 @@ const swiper4 = new Swiper('.swiper4', {
     }
   },*/
 });
-
-
-
-const swiper5 = new Swiper('.swiper5', {
-  // Optional parameters
-  direction: 'vertical',
-  loop: true,
-  //mousewheel: true,
-  touchRatio: 1,
-  //mousewheelControl: true,
-  // If we need pagination
-  pagination: {
-    el: '.swiper5-pagination',
-    clickable: true,
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper5-button-next',
-    prevEl: '.swiper5-button-prev',
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper5-scrollbar',
-  },
-
-  /*breakpoints: {
-    0: { // при 0px и выше
-      direction: 'horizontal', // горизонтальная прокрутка
-    },
-    768: { // при 768px и выше
-      direction: 'vertical', // вертикальная прокрутка
-    }
-  },*/
-});
-
 
 
 
