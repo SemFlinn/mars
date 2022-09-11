69cf431d0b135ce82e8192667014d099523a7213
SemFlinn <100510929+SemFlinn@users.noreply.github.com>
2022-09-11 18:54:29 +0300

    Update swiper.js

--- js/swiper.js
+++ js/swiper.js
@@ -1,4 +1,47 @@
+new Swiper('.swiper1', {
+  // Optional parameters
+  direction: 'vertical',
+  loop: true,
 
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
+
+
+new Swiper('.swiper2', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
 
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////gallery2
