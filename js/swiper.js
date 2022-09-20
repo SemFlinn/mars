516b8e38b274981ec1d5871634b95af9891aa706
SemFlinn <100510929+SemFlinn@users.noreply.github.com>
2022-09-20 19:50:23 +0300

    Add files via upload

---
+++ js/swiper.js
@@ -0,0 +1,23 @@
+const options = {
+  // root: document.querySelector( '#viewport' ), // я закомментил строку, чтобы использовать значение по умолчанию
+  rootMargin: '0px',
+  threshold: [ 0 ]
+};
+ 
+/*const observer = new IntersectionObserver( trueCallback, options );*/
+ 
+const target = document.querySelector( '.section6' );
+/*observer.observe( target ); // запускаем "слежку" за элементом(ами) в константе target*/
+ 
+// callback-функция (возвратная функция)
+const trueCallback = function(entries, observer) {
+  entries.forEach((entry) => {
+    // делаем что-либо для каждого переданного элемента (в нашем случае он один)
+    console.log( 'сработало' );
+    // например можно добавить какой-либо CSS-класс элементу
+    entry.target.classList.toggle( '_active' );
+  });
+}
+
+const observer = new IntersectionObserver( trueCallback, options );
+observer.observe( target ); // запускаем "слежку" за элементом(ами) в константе target
